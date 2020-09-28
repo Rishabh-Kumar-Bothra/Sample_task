96ff47f0c39bdb1c72c128391046064a53b2eb47
geekychasser <rbothra77@gmail.com>
2020-09-28 19:27:55 +0530

    added react-bootstrap

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,14 +1,17 @@
 import React from "react";
-import './Product.css'
+import Button from 'react-bootstrap/Button';
+import './Product.css';
 
 function Product(props){
     console.log(props.info.final_price)
     return(
         <div>
-            <h2>{props.info.name}</h2>
-            <h4>&#8377; {props.info.final_price}</h4>
+            <h4>{props.info.name}</h4>
+            <h5>&#8377; {props.info.final_price}</h5>
             <h6 className="oldPrice">&#8377; {props.info.price}</h6>
-            {props.info.is_in_stock ? <button>Add to cart</button> : <button>Out of stock</button>}
+            {props.info.is_in_stock ? 
+            <Button variant="outline-success">Add to cart</Button> : 
+            <Button variant="secondary">Out of stock</Button>}
         </div>
     )
 }
